75081db70b9b656e5ed2c4431ee0d0234b29fbb6
Harshavardhana <harsha@harshavardhana.net>
2015-09-17 16:25:38 -0700

    Bump to version 0.2.5

--- api-v2.go
+++ api-v2.go
@@ -178,7 +178,7 @@ type Config struct {
 // Global constants
 const (
 	LibraryName    = "minio-go"
-	LibraryVersion = "0.2.4"
+	LibraryVersion = "0.2.5"
 )
 
 // SetUserAgent - append to a default user agent
