40a4dd33243a4f543d0f43fa809edfc2352b00e4
Harshavardhana <harsha@harshavardhana.net>
2016-03-04 14:59:48 -0800

    retry: Implement transparent retry logic for all requests.
    Retry is transparent and retries upto maximum of '3' attempts
    in binomial time ie.
    
      1st attempt after 1sec.
      2nd attempt after 2sec.
      3th attempt after 4sec.
    
    Fixes #330

--- api-put-bucket.go
+++ api-put-bucket.go
@@ -1,5 +1,5 @@
 /*
- * Minio Go Library for Amazon S3 Compatible Cloud Storage (C) 2015 Minio, Inc.
+ * Minio Go Library for Amazon S3 Compatible Cloud Storage (C) 2015, 2016 Minio, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -180,19 +180,16 @@ func (c Client) SetBucketACL(bucketName string, acl BucketACL) error {
 		customHeader.Set("x-amz-acl", "private")
 	}
 
-	// Instantiate a new request.
-	req, err := c.newRequest("PUT", requestMetadata{
+	// Execute PUT bucket.
+	resp, err := c.executeMethod("PUT", requestMetadata{
 		bucketName:   bucketName,
 		queryValues:  urlValues,
 		customHeader: customHeader,
 	})
+	defer closeResponse(resp)
 	if err != nil {
 		return err
 	}
-
-	// Initiate the request.
-	resp, err := c.do(req)
-	defer closeResponse(resp)
 	if err != nil {
 		return err
 	}
