759d6d6fe8a77a2bedc692ea2f418b168fcae69b
Anis Elleuch <vadmeste@gmail.com>
2016-09-30 22:13:54 +0100

    Add documentation and example of MultiRemoveObjects API (#526)

---
+++ examples/s3/multiremoveobjects.go
@@ -0,0 +1,63 @@
+// +build ignore
+
+/*
+ * Minio Go Library for Amazon S3 Compatible Cloud Storage (C) 2015 Minio, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package main
+
+import (
+	"log"
+	"strconv"
+
+	"github.com/minio/minio-go"
+)
+
+func main() {
+	// Note: YOUR-ACCESSKEYID, YOUR-SECRETACCESSKEY, my-bucketname and my-objectname
+	// are dummy values, please replace them with original values.
+
+	// Requests are always secure (HTTPS) by default. Set secure=false to enable insecure (HTTP) access.
+	// This boolean value is the last argument for New().
+
+	// New returns an Amazon S3 compatible client object. API compatibility (v2 or v4) is automatically
+	// determined based on the Endpoint value.
+	s3Client, err := minio.New("s3.amazonaws.com", "YOUR-ACCESSKEYID", "YOUR-SECRETACCESSKEY", true)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	objectsCh := make(chan string)
+
+	// Send object names that are needed to be removed to objectsCh
+	go func() {
+		for i := 0; i < 10; i++ {
+			objectsCh <- "object" + strconv.Itoa(i)
+		}
+	}()
+
+	// Call MultiRemoveObjects API
+	errorCh, err := s3Client.MultiRemoveObjects("my-bucketname", objectsCh)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// Print errors received from MultiRemoveObjects API
+	for e := range errorCh {
+		log.Fatalln("Failed to remove " + e.ObjectName + ", error: " + e.Err.Error())
+	}
+
+	log.Println("Success")
+}
