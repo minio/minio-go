fdad76bdf4d11808a602d8cb3500d4bc53d21599
Harshavardhana <harsha@minio.io>
2017-06-23 22:43:48 -0700

    api: objectInfo filtering should include more headers. (#724)
    Fixes https://github.com/minio/mc/issues/2193

--- api-stat.go
+++ api-stat.go
@@ -55,11 +55,13 @@ func (c Client) BucketExists(bucketName string) (bool, error) {
 // List of header keys to be filtered, usually
 // from all S3 API http responses.
 var defaultFilterKeys = []string{
+	"Connection",
 	"Transfer-Encoding",
 	"Accept-Ranges",
 	"Date",
 	"Server",
 	"Vary",
+	"x-amz-bucket-region",
 	"x-amz-request-id",
 	"x-amz-id-2",
 	// Add new headers to be ignored.
