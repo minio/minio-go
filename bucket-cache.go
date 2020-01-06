6ee251e2eda44484e18beaad0bda6b44110bc14f
Harshavardhana <harsha@minio.io>
2020-01-06 13:52:32 -0800

    fix: AWS Snowball returns 501 handle it

--- bucket-cache.go
+++ bucket-cache.go
@@ -125,6 +125,8 @@ func processBucketLocationResponse(resp *http.Response, bucketName string) (buck
 			// request. Move forward and let the top level callers
 			// succeed if possible based on their policy.
 			switch errResp.Code {
+			case "NotImplemented":
+				fallthrough
 			case "AuthorizationHeaderMalformed":
 				fallthrough
 			case "InvalidRegion":
