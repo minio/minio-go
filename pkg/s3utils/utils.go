e9e301514c5e9aca3f96b991834ea5ce7b833e25
wuhanqing <wuhanqing@ruijie.com.cn>
2020-03-24 20:29:46 +0800

    unexport isAliyunOSSEndpoint

--- pkg/s3utils/utils.go
+++ pkg/s3utils/utils.go
@@ -78,7 +78,7 @@ func IsVirtualHostSupported(endpointURL url.URL, bucketName string) bool {
 		return false
 	}
 	// Return true for all other cases
-	return IsAmazonEndpoint(endpointURL) || IsGoogleEndpoint(endpointURL) || IsAliyunOSSEndpoint(endpointURL)
+	return IsAmazonEndpoint(endpointURL) || IsGoogleEndpoint(endpointURL) || isAliyunOSSEndpoint(endpointURL)
 }
 
 // Refer for region styles - https://docs.aws.amazon.com/general/latest/gr/rande.html#s3_region
@@ -135,11 +135,12 @@ func GetRegionFromURL(endpointURL url.URL) string {
 	return ""
 }
 
-func IsAliyunOSSEndpoint(endpointURL url.URL) bool {
+// isAliyunOSSEndpoint - Match if it is exactly Aliyun OSS endpoint.
+func isAliyunOSSEndpoint(endpointURL url.URL) bool {
 	if strings.HasSuffix(endpointURL.Host, "aliyuncs.com") {
 		return true
 	}
-	return GetRegionFromURL(endpointURL) != ""
+	return false 
 }
 
 // IsAmazonEndpoint - Match if it is exactly Amazon S3 endpoint.
