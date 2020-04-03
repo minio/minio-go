94f0286abd36e18113e7a6cb7bbb0dd41c17d067
Anis Elleuch <vadmeste@users.noreply.github.com>
2020-04-03 12:14:30 +0100

    retry: Quit when the context is cancelled (#1260)
    The retry goroutine can be stuck when sending a new
    retry number, this commit will listen for a context
    cancelltion to properly quit when the retry is canceled
    from the caller.

--- retry.go
+++ retry.go
@@ -70,7 +70,11 @@ func (c Client) newRetryTimer(ctx context.Context, maxRetry int, unit time.Durat
 	go func() {
 		defer close(attemptCh)
 		for i := 0; i < maxRetry; i++ {
-			attemptCh <- i + 1
+			select {
+			case attemptCh <- i + 1:
+			case <-ctx.Done():
+				return
+			}
 
 			select {
 			case <-time.After(exponentialBackoffWait(i)):
