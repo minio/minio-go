8808bc4806dbf55b34a2dc98da3b39993177df79
Krishnan Parthasarathi <krishnan.parthasarathi@gmail.com>
2021-08-09 16:48:01 -0700

    Accept ExpiredObjectDeleteMarker from lifecycle policy input as json

--- pkg/lifecycle/lifecycle_test.go
+++ pkg/lifecycle/lifecycle_test.go
@@ -19,9 +19,47 @@ package lifecycle
 
 import (
 	"encoding/json"
+	"reflect"
 	"testing"
 	"time"
 )
+
+func TestExpiredObjectDeleteMarker(t *testing.T) {
+	input := `{
+  "Rules": [
+    {
+      "ID": "expire-obj-delmarker",
+      "Status": "Enabled",
+      "Expiration": {
+        "ExpiredObjectDeleteMarker": true
+      }
+    }
+   ]
+   }`
+	var cfg Configuration
+	err := json.Unmarshal([]byte(input), &cfg)
+	if err != nil {
+		t.Fatalf("Failed to unmarshal lifecycle config %v", err)
+	}
+
+	if !cfg.Rules[0].Expiration.DeleteMarker {
+		t.Fatal("Expected Expiration.DeleteMarker to be true but got false")
+	}
+
+	// Round-trip test
+	b, err := json.Marshal(cfg)
+	if err != nil {
+		t.Fatalf("Failed to marshal lifecycle config %v", err)
+	}
+	var newcfg Configuration
+	err = json.Unmarshal(b, &newcfg)
+	if err != nil {
+		t.Fatalf("Failed to unmarshal lifecycle config json %v", err)
+	}
+	if !reflect.DeepEqual(newcfg, cfg) {
+		t.Fatalf("Expected configs to be equal but they aren't: newcfg %v cfg %v", newcfg, cfg)
+	}
+}
 
 func TestMarshalJSON(t *testing.T) {
 	testNow := time.Date(2021, time.January, 1, 0, 0, 0, 0, time.UTC)
@@ -78,7 +116,7 @@ func TestMarshalJSON(t *testing.T) {
 		},
 	}
 
-	expected := `{"Rules":[{"AbortIncompleteMultipartUpload":{"DaysAfterInitiation":1},"Expiration":{"Days":3,"DeleteMarker":false},"ID":"rule-1","Filter":{"Prefix":"prefix"},"Status":"Enabled"},{"Expiration":{"Date":"2021-01-01T00:00:00Z","DeleteMarker":false},"ID":"rule-2","Filter":{"And":{"Prefix":"prefix","Tags":[{"Key":"key-1","Value":"val-1"}]}},"NoncurrentVersionExpiration":{"NoncurrentDays":1},"Status":"Enabled"},{"Expiration":{"DeleteMarker":true},"ID":"rule-3","NoncurrentVersionTransition":{"NoncurrentDays":3},"Status":"Enabled","Transition":{"Days":3}}]}`
+	expected := `{"Rules":[{"AbortIncompleteMultipartUpload":{"DaysAfterInitiation":1},"Expiration":{"Days":3},"ID":"rule-1","Filter":{"Prefix":"prefix"},"Status":"Enabled"},{"Expiration":{"Date":"2021-01-01T00:00:00Z"},"ID":"rule-2","Filter":{"And":{"Prefix":"prefix","Tags":[{"Key":"key-1","Value":"val-1"}]}},"NoncurrentVersionExpiration":{"NoncurrentDays":1},"Status":"Enabled"},{"Expiration":{"ExpiredObjectDeleteMarker":true},"ID":"rule-3","NoncurrentVersionTransition":{"NoncurrentDays":3},"Status":"Enabled","Transition":{"Days":3}}]}`
 
 	got, err := json.Marshal(inp)
 	if err != nil {
