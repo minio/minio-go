397b33010d5f9a20cd93a142503732c081e472cd
Poorna Krishnamoorthy <poornas@users.noreply.github.com>
2021-09-15 12:31:07 -0400

    preserve backward compatibility for `mc replicate add` (#1546)

--- pkg/replication/replication_test.go
+++ pkg/replication/replication_test.go
@@ -153,7 +153,7 @@ func TestEditReplicationRule(t *testing.T) {
 		opts        Options
 		expectedErr string
 	}{
-		{ //test case :1
+		{ //test case :1 edit a rule in older config with remote ARN in destination bucket
 			cfg: Config{
 				Role: "arn:minio:replication:eu-west-1:c5acb6ac-9918-4dc6-8534-6244ed1a611a:destbucket",
 				Rules: []Rule{{
@@ -171,7 +171,7 @@ func TestEditReplicationRule(t *testing.T) {
 				StorageClass: "STANDARD",
 				DestBucket:   "arn:minio:replication:eu-west-1:c5acb6ac-9918-4dc6-8534-6244ed1a611a:destbucket",
 			},
-			expectedErr: "",
+			expectedErr: "invalid destination bucket for this rule",
 		},
 		{ //test case :2 mismatched rule id
 			cfg: Config{
@@ -296,6 +296,26 @@ func TestEditReplicationRule(t *testing.T) {
 				DestBucket:   "arn:aws:s3:::destbucket",
 			},
 			expectedErr: "priority must be unique. Replication configuration already has a rule with this priority",
+		},
+		{ //test case :8 ; edit a rule in older config
+			cfg: Config{
+				Role: "arn:minio:replication:eu-west-1:c5acb6ac-9918-4dc6-8534-6244ed1a611a:destbucket",
+				Rules: []Rule{{
+					ID:          "xyz.id",
+					Priority:    1,
+					Filter:      Filter{Prefix: "xyz/"},
+					Destination: Destination{Bucket: "arn:aws:s3:::destbucket"},
+				}}},
+			opts: Options{
+				ID:           "xyz.id",
+				Prefix:       "abc/",
+				RuleStatus:   "enable",
+				Priority:     "3",
+				TagString:    "k1=v1&k2=v2",
+				StorageClass: "STANDARD",
+				DestBucket:   "arn:aws:s3:::destbucket",
+			},
+			expectedErr: "",
 		},
 	}
 
