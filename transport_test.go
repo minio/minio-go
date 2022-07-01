a5374bb4a7bedd99c90f6e051a37abc7a5eeeea5
Klaus Post <klauspost@gmail.com>
2022-07-01 17:31:26 +0200

    Add non-failing tests.

--- transport_test.go
+++ transport_test.go
@@ -47,9 +47,43 @@ func Test_transportTimeoutWrapper_Response(t *testing.T) {
 	t.Log("timeout took", time.Since(started))
 }
 
+func Test_transportTimeoutWrapper_ResponseTrickle(t *testing.T) {
+	t.Parallel()
+	// We need a reasonably long timeout to ensure it is caught.
+	wait := time.Second
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		for i := 0; i < 4; i++ {
+			fmt.Fprintln(w, "Hello, client")
+			w.(http.Flusher).Flush()
+			time.Sleep(wait / 4)
+		}
+	}))
+	defer ts.Close()
+
+	client := http.Client{Transport: transportTimeoutWrapper(http.DefaultTransport, wait/2)}
+	req, err := http.NewRequest("GET", ts.URL, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	res, err := client.Do(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, err = io.ReadAll(res.Body)
+	if err != nil {
+		t.Fatal("got error", err)
+	}
+	err = res.Body.Close()
+	if err != nil {
+		t.Fatal("got error", err)
+	}
+}
+
 type fakeRoundtripper struct {
-	// Read this many bytes from input...
-	readFirst int
+	// This many times...
+	times int
+	// read this many bytes from input...
+	readEach int
 	// ... then wait this long.
 	wait time.Duration
 }
@@ -57,18 +91,20 @@ type fakeRoundtripper struct {
 func (f fakeRoundtripper) RoundTrip(request *http.Request) (*http.Response, error) {
 	ctx := request.Context()
 	defer request.Body.Close()
-	var buf = make([]byte, f.readFirst)
-	_, err := io.ReadFull(request.Body, buf)
-	if err != nil {
-		return nil, err
+	var buf = make([]byte, f.readEach)
+	for i := 0; i < f.times; i++ {
+		_, err := io.ReadFull(request.Body, buf)
+		if err != nil {
+			return nil, err
+		}
+		select {
+		case <-ctx.Done():
+			return nil, request.Context().Err()
+		case <-time.NewTimer(f.wait).C:
+		}
 	}
-	select {
-	case <-ctx.Done():
-		return nil, request.Context().Err()
-	case <-time.NewTimer(f.wait).C:
-	}
-	_, err = io.Copy(io.Discard, request.Body)
-	return nil, err
+	_, err := io.Copy(io.Discard, request.Body)
+	return &http.Response{}, err
 }
 
 func Test_transportTimeoutWrapper_Request(t *testing.T) {
@@ -79,7 +115,7 @@ func Test_transportTimeoutWrapper_Request(t *testing.T) {
 	const wait = time.Second
 	const size = 10000
 
-	transport := transportTimeoutWrapper(fakeRoundtripper{readFirst: size, wait: wait}, wait/2)
+	transport := transportTimeoutWrapper(fakeRoundtripper{readEach: size, wait: wait, times: 1}, wait/2)
 	client := http.Client{Transport: transport}
 
 	req, err := http.NewRequest("GET", "", bytes.NewBuffer(make([]byte, size*2)))
@@ -93,5 +129,27 @@ func Test_transportTimeoutWrapper_Request(t *testing.T) {
 	}
 	if !errors.Is(err, context.DeadlineExceeded) {
 		t.Fatal("expected timeout, got", err)
+	}
+}
+
+func Test_transportTimeoutWrapper_RequestTrickle(t *testing.T) {
+	// Seems like `httptest.NewServer` buffers bodies, so we cannot use it.
+	// Resort to manual testing
+	t.Parallel()
+	// We need a reasonably long timeout to ensure it is caught.
+	const wait = time.Second
+	const size = 10000
+
+	// Read every wait / 2, timeout is wait.
+	transport := transportTimeoutWrapper(fakeRoundtripper{readEach: size / 2, wait: wait / 2, times: 4}, wait)
+	client := http.Client{Transport: transport}
+
+	req, err := http.NewRequest("GET", "", bytes.NewBuffer(make([]byte, size*2)))
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, err = client.Do(req)
+	if err != nil {
+		t.Fatal("got error", err)
 	}
 }
