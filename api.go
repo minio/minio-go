6c7eee12e2b9e10d521ab088ba15585b7c4af51d
Klaus Post <klauspost@gmail.com>
2022-10-17 09:54:58 +0200

    Update api.go
    Co-authored-by: Harshavardhana <harsha@minio.io>

--- api.go
+++ api.go
@@ -255,7 +255,7 @@ func privateNew(endpoint string, opts *Options) (*Client, error) {
 		clnt.sha256Hasher = newSHA256Hasher
 	}
 
-	clnt.trailingHeaderSupport = opts.TrailingHeaders && !clnt.overrideSignerType.IsV4()
+	clnt.trailingHeaderSupport = opts.TrailingHeaders && clnt.overrideSignerType.IsV4()
 
 	// Sets bucket lookup style, whether server accepts DNS or Path lookup. Default is Auto - determined
 	// by the SDK. When Auto is specified, DNS lookup is used for Amazon/Google cloud endpoints and Path for all other endpoints.
