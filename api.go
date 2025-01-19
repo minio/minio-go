8b220d8e58cc4e1bbb667b732a130c7b647d8db9
Minio Trusted <trusted@minio.io>
2025-01-19 08:30:03 +0000

    Update version to next release

--- api.go
+++ api.go
@@ -133,7 +133,7 @@ type Options struct {
 // Global constants.
 const (
 	libraryName    = "minio-go"
-	libraryVersion = "v7.0.84"
+	libraryVersion = "v7.0.85"
 )
 
 // User Agent should always following the below style.
