55ca5dc850b85a02e1c327ddd8fff5fd4e511ed3
Simon Lukasik <slukasik@akamai.com>
2026-03-18 20:18:23 +0100

    Fix SetBucketLifecycle() for global rules
    https://docs.aws.amazon.com/AmazonS3/latest/API/API_LifecycleRule.html
    mentions that
    
           Filter is required
           if the LifecycleRule does not contain a Prefix element.
    
    After #2177, this requirement is broken for bucket-global (=prefixless)
    rules. And clients setting such may run into server side validation
    issues like.
    
    HTTP/1.1 400 Bad Request
    
    <?xml version="1.0" encoding="UTF-8"?>
    <Error>
      <Code>MalformedXML</Code>
      ...
    </Error>

--- pkg/lifecycle/lifecycle_test.go
+++ pkg/lifecycle/lifecycle_test.go
@@ -518,7 +518,6 @@ func TestLifecycleMarshalXML(t *testing.T) {
 			input: Configuration{
 				Rules: []Rule{
 					{
-
 						ID:                             "expire-incomplete-uploads-1",
 						Status:                         "Enabled",
 						Prefix:                         "my_dir",
@@ -543,8 +542,7 @@ func TestLifecycleMarshalXML(t *testing.T) {
 					},
 				},
 			},
-			// this is wrong fixed behavior fixed in a following commit
-			expectedXMLOut: "<LifecycleConfiguration><Rule><AbortIncompleteMultipartUpload><DaysAfterInitiation>1</DaysAfterInitiation></AbortIncompleteMultipartUpload><ID>expire-incomplete-uploads-2</ID><Status>Enabled</Status></Rule></LifecycleConfiguration>",
+			expectedXMLOut: "<LifecycleConfiguration><Rule><AbortIncompleteMultipartUpload><DaysAfterInitiation>1</DaysAfterInitiation></AbortIncompleteMultipartUpload><ID>expire-incomplete-uploads-2</ID><Filter><Prefix></Prefix></Filter><Status>Enabled</Status></Rule></LifecycleConfiguration>",
 		},
 		{
 			testDescription: "Ensure we always export Filter or Prefix. Specification explicitly mentions: 'Filter is required if the LifecycleRule does not contain a Prefix element.' (https://docs.aws.amazon.com/AmazonS3/latest/API/API_LifecycleRule.html)",
@@ -559,8 +557,7 @@ func TestLifecycleMarshalXML(t *testing.T) {
 					},
 				},
 			},
-			// this is wrong fixed behavior fixed in a following commit
-			expectedXMLOut: "<LifecycleConfiguration><Rule><AbortIncompleteMultipartUpload><DaysAfterInitiation>1</DaysAfterInitiation></AbortIncompleteMultipartUpload><ID>expire-incomplete-uploads-3</ID><Status>Enabled</Status></Rule></LifecycleConfiguration>",
+			expectedXMLOut: "<LifecycleConfiguration><Rule><AbortIncompleteMultipartUpload><DaysAfterInitiation>1</DaysAfterInitiation></AbortIncompleteMultipartUpload><ID>expire-incomplete-uploads-3</ID><Filter><Prefix></Prefix></Filter><Status>Enabled</Status></Rule></LifecycleConfiguration>",
 		},
 	}
 
