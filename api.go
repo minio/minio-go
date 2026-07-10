25eaec8fb1394915fefdd2e48b949882b780d94e
jiuker <jiuker@minio.io>
2026-07-10 12:15:31 +0800

    apply suggestion

--- api.go
+++ api.go
@@ -633,7 +633,9 @@ func (c *Client) do(req *http.Request) (resp *http.Response, err error) {
 
 	resp, err = c.httpClient.Do(req)
 	if err != nil {
-		_ = c.dumpHTTP(req, nil, err)
+		if c.isTraceEnabled {
+			_ = c.dumpHTTP(req, nil, err)
+		}
 		// Handle this specifically for now until future Golang versions fix this issue properly.
 		if urlErr, ok := err.(*url.Error); ok {
 			if strings.Contains(urlErr.Err.Error(), "EOF") {
